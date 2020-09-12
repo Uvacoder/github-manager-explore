62f6bcbe94b6f028b9aa49e3a08b0ac5e19bf1fe
Joao Medeiros <joaoserj@gmail.com>
2020-09-11 23:16:26 -0300

    fix: 🐛 added home button menu

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,11 +1,37 @@
-import { defineComponent } from '@vue/composition-api'
+import { defineComponent, reactive } from '@vue/composition-api'
 // import gql from 'graphql-tag'
 // import { useQuery } from '@vue/apollo-composable'
 
 export const Home = defineComponent({
   setup() {
+    const menu = reactive([
+      {
+        color: 'blue white--text',
+        icon: 'fas fa-shield-alt',
+        name: 'Vulnerability Alerts',
+        router: 'vulnerabilityalerts'
+      }
+    ])
     return () => {
-      return <div class={'ma-2'}></div>
+      return (
+        <div>
+          <v-card>
+            <h2 class="text-center">Menu</h2>
+            <v-row justify={'center'} align={'center'} class={'mx-0'}>
+              {menu.map((e) => {
+                return (
+                  <buttonMenu
+                    color={e.color}
+                    name={e.name}
+                    router={e.router}
+                    icon={e.icon}
+                  />
+                )
+              })}
+            </v-row>
+          </v-card>
+        </div>
+      )
     }
   }
 })
